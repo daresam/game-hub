1dfc94576849e2fe92c45380f541713a5ff094cc
Dare Samuel <dawakesam@gmail.com>
2023-07-08 00:53:23 +0100

    Filtered games by genre

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,10 +1,21 @@
-import { HStack, Image, List, ListItem, Spinner, Text } from "@chakra-ui/react";
-import useGenres from "../hooks/useGenres";
+import {
+  Button,
+  HStack,
+  Image,
+  List,
+  ListItem,
+  Spinner
+} from "@chakra-ui/react";
+import useGenres, { Genre } from "../hooks/useGenres";
 import getCroppedImageUrl from "../services/image-url";
 
-const GenreList = () => {
+interface Props {
+    onSelectedGenre: (genre: Genre) => void
+}
+
+const GenreList = ({onSelectedGenre}: Props) => {
   const { data, isLoading, error } = useGenres();
-  if (error) return;
+  if (error) return null;
   if (isLoading) return <Spinner />;
   return (
     <List>
@@ -16,7 +27,13 @@ const GenreList = () => {
               boxSize="32px"
               borderRadius={8}
             />
-            <Text fontSize="lg">{genre.name}</Text>
+            <Button
+              onClick={() => onSelectedGenre(genre)}
+              fontSize="lg"
+              variant="link"
+            >
+              {genre.name}
+            </Button>
           </HStack>
         </ListItem>
       ))}
--- src/hooks/useData.ts
+++ src/hooks/useData.ts
@@ -1,15 +1,16 @@
 import { useEffect, useState } from "react";
 import apiClient from "../services/api-client";
-import { CanceledError } from "axios";
+import { AxiosRequestConfig, CanceledError } from "axios";
 
 
-interface FetchResponse<T> {
+interface FetchResponse<T> { 
     count: number
     results: T[]
 }
 
 
-const useData = <T>(endpoint: string) => {
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+const useData = <T>(endpoint: string, requestConfig?: AxiosRequestConfig, deps?: any[]) => {
     const [data, setData] = useState<T[]>([]);
   const [error, setError] = useState("");
   const [isLoading, setLoading] = useState(false)
@@ -20,7 +21,7 @@ const useData = <T>(endpoint: string) => {
     setLoading(true);
 
     apiClient
-      .get<FetchResponse<T>>(endpoint, { signal: controller.signal })
+      .get<FetchResponse<T>>(endpoint,  { signal: controller.signal, ...requestConfig,  })
       .then((res) => {
         setData(res.data.results)
         setLoading(false)
@@ -32,7 +33,8 @@ const useData = <T>(endpoint: string) => {
       });
 
     return () => controller.abort();
-  }, []);
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, deps ? [...deps] : []);
 
   return {
     data,
